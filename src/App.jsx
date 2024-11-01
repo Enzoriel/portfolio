d53e74721202ae8bade7e5630db0fcfc3f12e9d7
Enzorieldev <enzorieldev@outlook.com>
2024-11-01 15:21:03 -0300

    Creando portfolio

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="html">
+      <nav className="nav">
+        <section className="logo">
+          <img className="logo-img" src="../../public/my-logo.svg" />
+        </section>
+
+        <ul className="nav-list">
+          <li className="nav-item">Inicio</li>
+          <li className="nav-item">Proyectos</li>
+          <li className="nav-item">Tecnologías</li>
+          <li className="nav-item">Contacto</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
