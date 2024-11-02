22c2957eaae9fd169357950e981afab2f19d37dc
Enzorieldev <enzorieldev@outlook.com>
2024-11-01 23:14:27 -0300

    Estilización y creando presentación

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,41 @@ function App() {
   return (
     <div className="html">
       <nav className="nav">
-        <section className="logo">
-          <img className="logo-img" src="../../public/my-logo.svg" />
+        <section className="logo class-box">
+          <a>
+            <img className="logo-img" src="../../public/my-logo.svg" />
+          </a>
         </section>
 
-        <ul className="nav-list">
-          <li className="nav-item">Inicio</li>
-          <li className="nav-item">Proyectos</li>
-          <li className="nav-item">Tecnologías</li>
-          <li className="nav-item">Contacto</li>
+        <ul className="nav-list class-box">
+          <li className="nav-item">
+            <a>Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a>Proyectos</a>
+          </li>
+          <li className="nav-item">
+            <a>Tecnologías</a>
+          </li>
+          <li className="nav-item">
+            <a>Contacto</a>
+          </li>
         </ul>
       </nav>
+      <section className="info class-box">
+        <div className="box-img">
+          <img className="img" src="../../public/logo-img.png" />
+        </div>
+        <div className="description">
+          <p>Hola, soy Enzorieldev.</p>
+          <p>Desarrollador front-end apasionado por crear experiencias interactivas y funcionales en la web. </p>
+          <p>Tengo experiencia construyendo interfaces de usuario dinámicas y reactivas.</p>
+          <p>
+            Mi enfoque está en optimizar cada proyecto para brindar una experiencia fluida y atractiva para los
+            usuarios.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
