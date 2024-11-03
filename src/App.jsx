6fad20e66cdb10df4e463cd272ce8fe2549ba8e8
Enzorieldev <enzorieldev@outlook.com>
2024-11-02 23:27:21 -0300

    Añadiendo secciones y estilizando

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,27 @@ function App() {
           </p>
         </div>
       </section>
+      <section className="proyect class-box">
+        <img src="../../public/proyect.svg" className="proyect-img" />
+        <div>
+          <div className="proyect-list">
+            <div className="proyect-item">
+              <h3>Valorant API</h3>
+              <img src="../../public/Valorant 1.png" />
+              <p>
+                Count the time your children spend on activities like homework, chores and exercise and convert it into
+                screen time. I created this with Vue.js and the Howlr library to add the interactive clock sounds. It
+                can be installed as a progressive web application. The git repository for this one is private at the
+                moment.
+              </p>
+              <p>Tecnoligias</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="tecnologies class-box">
+        <img src="../../tecnologies.svg" className="tecnologies-img" />
+      </section>
     </div>
   );
 }
