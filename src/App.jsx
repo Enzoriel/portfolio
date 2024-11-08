ce50bf005c7edee80c85c318ccc514133ff5a2b1
Enzorieldev <enzorieldev@outlook.com>
2024-11-08 17:17:49 -0300

    Agregando proyectos

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,51 @@ const Proyects = () => {
       {bd.map((element) => (
         <div className="proyect-item" key={element.name}>
           <h3>{element.name}</h3>
-          <img src={element.img} />
+          <img className="proyect-preview" src={element.img} />
           <p>{element.description}</p>
-          <p>{element.github + element.deploy}</p>
+          <div className="proyect-links">
+            <a
+              href={element.github}
+              title="Ver repositorio"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Ver código en Github"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="links"
+                src="/github.svg"
+                alt=""
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+              </svg>
+            </a>
+            <a href={element.deploy} title="Ver preview" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="links"
+                src="/preview.svg"
+                alt="Preview"
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 5a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1v-10z" />
+                <path d="M7 20h10" />
+                <path d="M9 16v4" />
+                <path d="M15 16v4" />
+              </svg>
+            </a>
+          </div>
         </div>
       ))}
     </div>
